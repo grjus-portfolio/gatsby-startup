05afdeac12e081cd7f1b7be232f0b45ab81886af
grjus <grjus@pm.me>
2021-04-19 18:15:28 +0200

    Updated header section

--- gatsby_startup/src/components/header.js
+++ gatsby_startup/src/components/header.js
@@ -1,29 +1,19 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import "./header.css"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#333`,
-      marginBottom: `0`,
-    }}
-  >
+  <header className="header-main">
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        maxWidth: "max-content",
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+      <h1>
+        <Link className="header-title" to="/">
           {siteTitle}
         </Link>
       </h1>
