ef6916d665eddd2081c638e99ccf7710c2e136ed
grjus <grjus@pm.me>
2021-04-19 18:15:03 +0200

    Added comments from json placeholer

--- gatsby_startup/gatsby-node.js
+++ gatsby_startup/gatsby-node.js
@@ -41,4 +41,57 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       component: postTemplate,
     })
   })
+
+  const commentTemplate = require.resolve("./src/templates/comment.js")
+
+  const resultComment = await graphql(`
+    {
+      allJsonPlaceholder {
+        edges {
+          node {
+            name
+            email
+            id
+            path
+          }
+        }
+      }
+    }
+  `)
+
+  if (resultComment.error) {
+    reporter.panicOnBuild("Error while running GraphQL query")
+    return
+  }
+  resultComment.data.allJsonPlaceholder.edges.forEach(({ node }) => {
+    createPage({
+      path: node.path,
+      component: commentTemplate,
+    })
+  })
+}
+
+const fetch = require(`node-fetch`)
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) => {
+  // get data from GitHub API at build time
+  const result = await fetch(`https://jsonplaceholder.typicode.com/comments`)
+  const data = await result.json()
+  data.forEach(item => {
+    const commentNode = {
+      id: createNodeId(`${item.id}`),
+      name: item.name,
+      email: item.email,
+      body: item.body,
+      path: `/data-fetch/${createNodeId(`${item.id}`)}`,
+      internal: {
+        type: `json_placeholder`,
+        contentDigest: createContentDigest(item),
+      },
+    }
+    actions.createNode(commentNode)
+  })
 }
